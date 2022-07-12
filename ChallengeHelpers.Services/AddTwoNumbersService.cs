47894ae553b15c909938d69b6f28ce27e97a68e2
htphong24 <htphong24@gmail.com>
2022-07-12 10:53:38 -0700

    Added AddTwoNumbers service

---
+++ ChallengeHelpers.Services/AddTwoNumbersService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChallengeHelpers.Services
+{
+    public class AddTwoNumbersService : ServiceBase<AddTwoNumbersRequest, ListNode>
+    {
+        /*
+         * INPUT:                      => OUTPUT:
+         * [2,4,3] + [5,6,4]           => 342 + 465      = 807         => [7,0,8]
+         * [0] + [0]                   => 0 + 0          = 0           => [0]
+         * [2,4,9] + [5,6,4,9]         => 942 + 9465     = 10407       => [7,0,4,0,1]
+         * [9,9,9,9,9,9,9] + [9,9,9,9] => 9999999 + 9999 = 10009998    => [8,9,9,9,0,0,0,1]
+         * [9] + [1,9,9,9,9,9,9,9,9,9] => 9 + 9999999991  = 10000000000 => [0,0,0,0,0,0,0,0,0,0,1] 
+         * */
+
+
+        protected override ListNode DoRun(AddTwoNumbersRequest rq)
+        {
+            // Test data
+            //rq.L1 = new ListNode().AddNodes(new[] { 2, 4, 3 });
+            //rq.L2 = new ListNode().AddNodes(new[] { 5, 6, 4 });
+
+            var l1 = rq.L1;
+            var l2 = rq.L2;
+
+            var currentNode = new ListNode(0); // Dummy node 
+            var head = currentNode; // store the head node so we can return it as the result
+            var carry = 0;
+
+            while (l1 != null || l2 != null)
+            {
+                var sum = carry;
+
+                if (l1 != null)
+                {
+                    // Add L1's value to sum
+                    sum += l1.val;
+                    // L1 moves to next node
+                    l1 = l1.next;
+                }
+
+                if (l2 != null)
+                {
+                    // Add L2's value to sum
+                    sum += l2.val;
+                    // L2 moves to next node
+                    l2 = l2.next;
+                }
+
+                // Determine values of carry & sum
+                carry = sum / 10;
+                sum %= 10;
+
+                // Create new node & link it to currentNode
+                currentNode.next = new ListNode(sum);
+                // currentNode moves to next node
+                currentNode = currentNode.next;
+            }
+
+            // Determine whether we should create last node
+            if (carry > 0)
+            {
+                currentNode.next = new ListNode(carry);
+                // No need to move currentNode to next node as it's the last node
+            }
+
+            head = head.next; // Remove dummy node (0)
+
+            return head;
+        }
+
+    }
+
+    public class AddTwoNumbersRequest
+    {
+        public AddTwoNumbersRequest(ListNode l1, ListNode l2)
+        {
+            L1 = l1;
+            L2 = l2;
+        }
+
+        public ListNode L1 { get; set; }
+
+        public ListNode L2 { get; set; }
+    }
+}
--- ChallengeHelpers.Tests/CheckPalindromeLinkedListServiceTests.cs
+++ ChallengeHelpers.Tests/CheckPalindromeLinkedListServiceTests.cs
@@ -11,50 +11,16 @@ namespace ChallengeHelpers.Tests
         public void DoRun_GivenNormalInputs_ReturnsExpectedResult()
         {
             // Arrange
-
-            // [1,7,3,3,6,5,6,3,3,7,1]
-            var node0010A = new ListNode(1); // last node
-            var node0010B = new ListNode(7, node0010A);
-            var node0010C = new ListNode(3, node0010B);
-            var node0010D = new ListNode(3, node0010C);
-            var node0010E = new ListNode(6, node0010D);
-            var node0010F = new ListNode(5, node0010E);
-            var node0010G = new ListNode(6, node0010F);
-            var node0010H = new ListNode(3, node0010G);
-            var node0010I = new ListNode(3, node0010H);
-            var node0010J = new ListNode(7, node0010I);
-            var node0010K = new ListNode(1, node0010J);
-
-            // [1,7,3,3,6,5,5,6,3,3,7,1]
-            var node0020A = new ListNode(1); // last node
-            var node0020B = new ListNode(7, node0020A);
-            var node0020C = new ListNode(3, node0020B);
-            var node0020D = new ListNode(3, node0020C);
-            var node0020E = new ListNode(6, node0020D);
-            var node0020F = new ListNode(5, node0020E);
-            var node0020G = new ListNode(5, node0020F);
-            var node0020H = new ListNode(6, node0020G);
-            var node0020I = new ListNode(3, node0020H);
-            var node0020J = new ListNode(3, node0020I);
-            var node0020K = new ListNode(7, node0020J);
-            var node0020L = new ListNode(1, node0020K);
-
-            // [1,7,1]
-            var node0030A = new ListNode(1); // last node
-            var node0030B = new ListNode(7, node0030A);
-            var node0030C = new ListNode(1, node0030B);
-
-            // [1,7,8,1]
-            var node0040A = new ListNode(1); // last node
-            var node0040B = new ListNode(8, node0040A);
-            var node0040C = new ListNode(7, node0040B);
-            var node0040D = new ListNode(1, node0040C);
+            var node0010 = new ListNode().AddNodes(new[] { 1, 7, 3, 3, 6, 5, 6, 3, 3, 7, 1 });
+            var node0020 = new ListNode().AddNodes(new[] { 1, 7, 3, 3, 6, 5, 5, 6, 3, 3, 7, 1 });
+            var node0030 = new ListNode().AddNodes(new[] { 1, 7, 1 });
+            var node0040 = new ListNode().AddNodes(new[] { 1, 7, 8, 1 });
 
             // Act
-            var r0010 = new CheckPalindromeLinkedListService().Run(new ListNode().AddNodes(new[] { 1, 7, 3, 3, 6, 5, 6, 3, 3, 7, 1 }), out var t0010);
-            var r0020 = new CheckPalindromeLinkedListService().Run(new ListNode().AddNodes(new[] { 1, 7, 3, 3, 6, 5, 5, 6, 3, 3, 7, 1 }), out var t0020);
-            var r0030 = new CheckPalindromeLinkedListService().Run(new ListNode().AddNodes(new[] { 1, 7, 1 }), out var t0030);
-            var r0040 = new CheckPalindromeLinkedListService().Run(new ListNode().AddNodes(new[] { 1, 7, 8, 1 }), out var t0040);
+            var r0010 = new CheckPalindromeLinkedListService().Run(node0010, out var t0010);
+            var r0020 = new CheckPalindromeLinkedListService().Run(node0020, out var t0020);
+            var r0030 = new CheckPalindromeLinkedListService().Run(node0030, out var t0030);
+            var r0040 = new CheckPalindromeLinkedListService().Run(node0040, out var t0040);
 
             // Assert
             Assert.AreEqual(r0010, true);
@@ -67,17 +33,12 @@ namespace ChallengeHelpers.Tests
         public void DoRun_GivenEdgeCases_ReturnsExpectedResult()
         {
             // Arrange
-
-            // [7]
-            var node0050A = new ListNode(7);
-
-            // [7,0]
-            var node0060A = new ListNode(7); // last node
-            var node0060B = new ListNode(0, node0060A);
+            var node0050 = new ListNode().AddNodes(new[] { 7 });
+            var node0060 = new ListNode().AddNodes(new[] { 7, 0 });
 
             // Act
-            var r0050 = new CheckPalindromeLinkedListService().Run(new ListNode().AddNodes(new[] { 7 }), out var t0050);
-            var r0060 = new CheckPalindromeLinkedListService().Run(new ListNode().AddNodes(new[] { 7, 0 }), out var t0060);
+            var r0050 = new CheckPalindromeLinkedListService().Run(node0050, out var t0050);
+            var r0060 = new CheckPalindromeLinkedListService().Run(node0060, out var t0060);
 
             // Assert
             Assert.AreEqual(r0050, true);
